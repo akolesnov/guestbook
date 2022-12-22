aba6029fa331625e100f37bfdcdc38760332e55b
akolesnov <a.kolesnov@yandex.ru>
2022-12-22 17:56:43 +0300

    added Chatting with Admins

--- config/secrets/dev/dev.list.php
+++ config/secrets/dev/dev.list.php
@@ -1,5 +1,6 @@
 <?php
 
 return [
-    'AKISMET_KEY' => "0bd726c8bbdc",
+    'AKISMET_KEY' => '0bd726c8bbdc',
+    'SLACK_DSN' => null,
 ];
--- src/Message/CommentMessage.php
+++ src/Message/CommentMessage.php
@@ -5,16 +5,23 @@ class CommentMessage
 {
     private $id;
     private $context;
+    private $reviewUrl;
 
-    public function __construct(int $id, array $context = [])
+    public function __construct(int $id, string $reviewUrl, array $context = [])
     {
         $this->id = $id;
+        $this->reviewUrl = $reviewUrl;
         $this->context = $context; 
     }
 
     public function getId(): int
     {
         return $this->id;
+    }
+
+    public function getReviewUrl(): string
+    {
+        return $this->reviewUrl;
     }
 
     public function getContext(): array
